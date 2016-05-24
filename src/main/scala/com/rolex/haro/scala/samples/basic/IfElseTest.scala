36a1fbe663864c27178ae4e4ac198a69a4100a3f
rolex <zengronghui@chinadaas.com>
2016-05-24 23:45:21 +0800

    scala samples if语句

---
+++ src/main/scala/com/rolex/haro/scala/samples/basic/IfElseTest.scala
@@ -0,0 +1,40 @@
+package com.rolex.haro.scala.samples.basic
+
+/**
+  * if else 语句.
+  * User: rolex
+  * Date: 2016/05/14
+  * Version: 1.0
+  */
+class IfElseTest {
+
+}
+
+object IfElseTest {
+    // if
+    if (true) {
+
+    }
+
+    // if...else...
+    if (true) {
+
+    } else {
+
+    }
+
+    // if...else if...else
+
+    if (true) {
+
+    } else if (true) {
+
+    }
+
+    // if...else 嵌套
+    if (true) {
+        if (true) {
+
+        }
+    }
+}
