e29a2fa247bb3b4c36f2090c59d843ec6e4e5420
rolex <zengronghui@chinadaas.com>
2016-05-25 22:19:48 +0800

    scala samples 元组

--- src/main/scala/com/rolex/haro/scala/samples/basic/TupleTest.scala
+++ src/main/scala/com/rolex/haro/scala/samples/basic/TupleTest.scala
@@ -6,20 +6,29 @@ package com.rolex.haro.scala.samples.basic
   * Date: 2016/3/21
   * version: 1.0
   */
+class TupleTest {
+
+}
+
 object TupleTest {
 
     def main(args: Array[String]) {
-        val tuple4 = (1,"2",3,"4")
+        val tuple4 = (1, "2", 3, "4")
 
-        print(tuple4._2)
+        println(tuple4._2) // 取值
+
+        // 元组中包含不同类型的数据
+        val tuple = (1, 2, 3, "java", "scala")
+
+        // 元组可反向赋值
+        val (one, two, three, four, _) = tuple
+
+        println("Java SDK".partition(_.isUpper))
+
+
+        val tuple1 = new Tuple2(1, 2) // 交换元素
+        println("tuple1 = " + tuple1.swap)
     }
 
-    // 元组中包含不同类型的数据
-    val tuple = (1,2,3,"java","scala")
 
-    // 元组可反向赋值
-    val (one, two, three, four, _) = tuple
-
-    println("Java SDK".partition(_.isUpper))
-    
 }
